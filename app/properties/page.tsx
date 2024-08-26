2e71a442689ceb0b542b9a87d9d45f37a394675b
Marwen Tahouri <marwentahouri2@gmail.com>
2024-08-26 12:37:40 +0100

    properties page

---
+++ app/properties/page.tsx
@@ -0,0 +1,43 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getListings from "../actions/getListings";
+import ClientOnly from "../components/ClientOnly";
+import EmptyState from "../components/EmptyState";
+import PropertiesClient from "./PropertiesClient";
+
+const PropertiesPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="Unauthorized"
+          subtitle="You need to be signed in to view this page."
+        />
+      </ClientOnly>
+    );
+  }
+
+  const listings = await getListings({
+    userId: currentUser.id,
+  });
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No Properties"
+          subtitle="You have no properties yet."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <PropertiesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default PropertiesPage;
